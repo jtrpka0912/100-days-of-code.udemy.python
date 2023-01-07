0c1169e7044543142f0adfbb1eab502c74cfd5ae
J. Trpka <jtrpka0912@gmail.com>
2023-01-07 13:44:50 -0500

    Challenge 4

--- day-007/main.py
+++ day-007/main.py
@@ -1,8 +1,69 @@
 import random
 
-word_list = ["aardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
+#Set 'lives' to equal 6.
+
+lives = 6
 
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -11,12 +72,10 @@ display = []
 for _ in chosen_word:
   display += '_'
 
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
-game_over = False
+did_game_end = False
 did_player_win = False
 
-while not game_over:
+while not did_game_end:
 
   guess = input('Guess a letter: ').lower()
 
@@ -24,14 +83,25 @@ while not game_over:
     if guess == chosen_word[index]:
       display[index] = guess
 
+  #TODO-2: - If guess is not a letter in the chosen_word,
+    #Then reduce 'lives' by 1. 
+    #If lives goes down to 0 then the game should stop and it should print "You lose."
+  if guess not in chosen_word:
+    lives -= 1
+
   if '_' not in display:
-    game_over = True
+    did_game_end = True
     did_player_win = True
+
+  if lives == 0:
+    did_game_end = True
+
+  #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+  print(stages[lives])
 
   print(display)
 
-if game_over:
-  if did_player_win:
-    print('You win')
-  else:
-    print('You lose')
+if did_player_win:
+  print('You win')
+else:
+  print('You lose')
