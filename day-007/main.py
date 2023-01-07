a1d62cc2ce30e88feb09f8fe2049e329dfdb8029
J. Trpka <jtrpka0912@gmail.com>
2023-01-07 14:02:11 -0500

    Challenge 5.

--- day-007/main.py
+++ day-007/main.py
@@ -1,92 +1,43 @@
+from hangman_art import stages, logo
+from hangman_words import word_list
 import random
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-
-#TODO-1: - Create a variable called 'lives' to keep track of the number of lives left. 
-#Set 'lives' to equal 6.
-
 lives = 6
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+print(logo)
+
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for _ in chosen_word:
   display += '_'
 
+guessed_letters = []
+
 did_game_end = False
 did_player_win = False
+
+print(stages[lives])
+print(display)
 
 while not did_game_end:
 
   guess = input('Guess a letter: ').lower()
 
+  if guess in guessed_letters:
+    print(f'You already guessed {guess}')
+    continue
+  else:
+    guessed_letters += guess
+
   for index in range(len(chosen_word)):
     if guess == chosen_word[index]:
       display[index] = guess
 
-  #TODO-2: - If guess is not a letter in the chosen_word,
-    #Then reduce 'lives' by 1. 
-    #If lives goes down to 0 then the game should stop and it should print "You lose."
   if guess not in chosen_word:
+    print(f'{guess} is not part of the word')
     lives -= 1
 
   if '_' not in display:
@@ -96,7 +47,6 @@ while not did_game_end:
   if lives == 0:
     did_game_end = True
 
-  #TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
   print(stages[lives])
 
   print(display)
