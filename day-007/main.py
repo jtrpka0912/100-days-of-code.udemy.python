5661a57eec4849abf2c718c1375f35f22f7f9ed1
J. Trpka <jtrpka0912@gmail.com>
2023-01-07 13:19:02 -0500

    Challenge 2.

--- day-007/main.py
+++ day-007/main.py
@@ -2,14 +2,19 @@ import random
 
 word_list = ["aardvark", "baboon", "camel"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in chosen_word:
+  display += '_'
+
 guess = input('Guess a letter: ').lower()
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-if guess in chosen_word:
-  print('Yes')
-else:
-  print('No')
+for index in range(len(chosen_word)):
+  if guess == chosen_word[index]:
+    display[index] = guess
+
+print(display)
