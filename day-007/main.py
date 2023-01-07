76d9d5ef0f45369e4eff364a37117ac62276ef89
J. Trpka <jtrpka0912@gmail.com>
2023-01-07 13:32:45 -0500

    Challenge 3.

--- day-007/main.py
+++ day-007/main.py
@@ -11,10 +11,27 @@ display = []
 for _ in chosen_word:
   display += '_'
 
-guess = input('Guess a letter: ').lower()
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
 
-for index in range(len(chosen_word)):
-  if guess == chosen_word[index]:
-    display[index] = guess
+game_over = False
+did_player_win = False
 
-print(display)
+while not game_over:
+
+  guess = input('Guess a letter: ').lower()
+
+  for index in range(len(chosen_word)):
+    if guess == chosen_word[index]:
+      display[index] = guess
+
+  if '_' not in display:
+    game_over = True
+    did_player_win = True
+
+  print(display)
+
+if game_over:
+  if did_player_win:
+    print('You win')
+  else:
+    print('You lose')
