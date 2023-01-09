a1888fe049a4241af7440b66d89c38fd816e3daf
J. Trpka <jtrpka0912@gmail.com>
2023-01-08 20:16:33 -0500

    Challenge 3.

--- day-008/main.py
+++ day-008/main.py
@@ -4,43 +4,25 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypt(text, shift):
-  encryption = ''
+def caesar(text, shift, direction):
+  new_word = ''
   for letter in text:
     letter_index = alphabet.index(letter)
 
-    shifted_index = letter_index + shift
+    if direction == 'encode':
+      shifted_index = letter_index + shift
+    elif direction == 'decode':
+      shifted_index = letter_index - shift
 
     if shifted_index >= (len(alphabet)):
       shifted_index -= len(alphabet)
 
-    encryption += alphabet[shifted_index]
-  
-  print(f'The encoded text is {encryption}')
+    new_word += alphabet[shifted_index]
 
-#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
-def decrypt(text, shift):
-  decryption = ''
+  if direction == 'encode':
+    print(f'The encoded text is {new_word}')
+  elif direction == 'decode':
+    print(f'The decoded text is {new_word}')
 
-  for letter in text:
-    letter_index = alphabet.index(letter)
 
-    shifted_index = letter_index - shift
-
-    decryption += alphabet[shifted_index]
-
-  print(f'The decoded text is {decryption}')
-
-  #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.  
-  #e.g. 
-  #cipher_text = "mjqqt"
-  #shift = 5
-  #plain_text = "hello"
-  #print output: "The decoded text is hello"
-
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
-
-if direction == 'encode':
-  encrypt(text, shift)
-elif direction == 'decode':
-  decrypt(text, shift)
+caesar(text, shift, direction)
