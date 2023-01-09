e793a65d93f7de38abc745ae9eb18bf4cdb77058
J. Trpka <jtrpka0912@gmail.com>
2023-01-08 20:36:16 -0500

    Final challenge.

--- day-008/main.py
+++ day-008/main.py
@@ -1,28 +1,44 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def caesar(text, shift, direction):
   new_word = ''
-  for letter in text:
-    letter_index = alphabet.index(letter)
+  for character in text:
+    if character in alphabet:
 
-    if direction == 'encode':
-      shifted_index = letter_index + shift
-    elif direction == 'decode':
-      shifted_index = letter_index - shift
+      letter_index = alphabet.index(character)
 
-    if shifted_index >= (len(alphabet)):
-      shifted_index -= len(alphabet)
+      if direction == 'encode':
+        shifted_index = letter_index + shift
+      elif direction == 'decode':
+        shifted_index = letter_index - shift
 
-    new_word += alphabet[shifted_index]
+      new_word += alphabet[shifted_index]
+    else:
+      new_word += character
 
   if direction == 'encode':
     print(f'The encoded text is {new_word}')
   elif direction == 'decode':
     print(f'The decoded text is {new_word}')
 
+print(logo)
 
-caesar(text, shift, direction)
+# Something to start the while loop
+repeat = 'yes'
+
+while repeat == 'yes':
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  # Had to look at the challenge answer
+  shift = shift % 26
+  
+  caesar(text, shift, direction)
+
+  repeat = input('Do you want to repeat? Type yes if you do.\n')
+
+print('Goodbye')
+
